210057259f57b99dce18c84b3b63537b47976c0d
Andy Chambers <andy.chambers@exoscale.ch>
2020-06-16 17:29:59 +0100

    Add tests to prove that we play nicely with schema registry

--- src/cddr/edn_avro.clj
+++ src/cddr/edn_avro.clj
@@ -28,26 +28,64 @@
 (defn as-edn
   "Converts a `GenericDatum` into EDN using a JsonEncoder"
   ([avro]
-   (as-edn avro {}))
-  ([avro {:keys [schema]}]
-   (let [schema (or schema
-                    (.getSchema avro))
-         output (ByteArrayOutputStream.)
-         writer (GenericDatumWriter. schema)
-         encoder (.jsonEncoder (EncoderFactory/get) schema output)]
+   (as-edn avro nil))
+  ([avro schema]
+   (let [write-schema (.getSchema avro)
+         read-schema schema]
 
-     (.write writer avro encoder)
-     (.flush encoder)
-     (.flush output)
+     ;; either way, we first need to write out the avro object
+     ;; to a ByteArray using the json encoder
+     (let [output (ByteArrayOutputStream.)
+           writer (GenericDatumWriter. write-schema)
+           encoder (.jsonEncoder (EncoderFactory/get) write-schema output)]
 
-     (json/read-str (String. (.toByteArray output))))))
+       (.write writer avro encoder)
+       (.flush encoder)
+       (.flush output)
 
+       (if read-schema
+         ;; if we're using a custom reader schema, we need to
+         ;; read it back in again using the GenericDatumReader
+         ;; configured with both the write and read schemas
+         (let [json-in (-> output
+                           .toByteArray
+                           ByteArrayInputStream.
+                           DataInputStream.)
+               decoder (.jsonDecoder (DecoderFactory/get) read-schema json-in)
+               reader (GenericDatumReader. write-schema read-schema)]
+           (as-edn (.read reader nil decoder)))
+         ;; if we're not using a custom read-schema, we can just
+         ;; parse the encoded json
+         (json/read-str (String. (.toByteArray output))))))))
 
 (comment
-  (let [s (avro-schema {:type "record"
-                        :name "Foo"
-                        :fields [{:name "yolo"
-                                  :type ["string" "null"]}]})]
-    (-> (as-avro {:yolo {:string "yolo"}} {:schema s})
+  ;; roundtrip a record (optionally using a custom reader schema)
+  (let [s1 (avro-schema {:type "record"
+                         :name "Foo"
+                         :fields [{:name "yolo"
+                                   :type ["string" "null"]}]})
+        s2 (avro-schema {:type "record"
+                         :name "Foo"
+                         :fields [{:name "yolo"
+                                   :type ["string" "null"]}
+                                  {:name "abc"
+                                   :type ["null" "long"]
+                                   :default nil}]})
+
+        avit (as-avro {:yolo {:string "yolo"}} s1)]
+
+    (-> avit
+        (as-edn s2))
+    ;; {"yolo" {"string" "yolo"}, "abc" nil}
+
+    (-> avit
+        (as-edn))
+    ;; {"yolo" {"string" "yolo"}}
+    )
+
+
+  (let [s1 (avro-schema "string")
+        avit (as-avro "yolo" s1)]
+    (-> avit
         (as-edn)))
   )
--- test/cddr/edn_avro_test.clj
+++ test/cddr/edn_avro_test.clj
@@ -1,7 +1,53 @@
 (ns cddr.edn-avro-test
-  (:require [clojure.test :refer :all]
-            [cddr.edn-avro :refer :all]))
+  (:require
+   [clojure.test :refer :all]
+   [cddr.edn-avro :as ea])
+  (:import
+   (org.apache.avro.generic GenericData$Record)
+   (io.confluent.kafka.serializers KafkaAvroSerializer KafkaAvroDeserializer)
+   (io.confluent.kafka.schemaregistry.client MockSchemaRegistryClient)))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-kafka-avro-serializer
+  (testing "can serialize record"
+    (let [schema (ea/avro-schema {:type "record"
+                                  :name "Foo"
+                                  :fields [{:name "yolo"
+                                            :type ["string" "null"]}]})
+          mock-reg (MockSchemaRegistryClient.)
+          serializer (KafkaAvroSerializer. mock-reg {"schema.registry.url" "test.reg"})
+          msg (ea/as-avro {:yolo {:string "yolo"}} schema)]
+
+      (is (.serialize serializer "yolo" msg))))
+
+  (testing "can serialize primitive"
+    (let [schema (ea/avro-schema "string")
+          mock-reg (MockSchemaRegistryClient.)
+          serializer (KafkaAvroSerializer. mock-reg {"schema.registry.url" "test.reg"})
+          msg (ea/as-avro "yolo" schema)]
+
+      (is (.serialize serializer "yolo" msg)))))
+
+(deftest test-kafka-avro-deserializer
+  (testing "can deserialize record"
+    (let [schema (ea/avro-schema {:type "record"
+                                  :name "Foo"
+                                  :fields [{:name "yolo"
+                                            :type ["string" "null"]}]})
+          mock-reg (MockSchemaRegistryClient.)
+          serializer (KafkaAvroSerializer. mock-reg {"schema.registry.url" "test.reg"})
+          deserializer (KafkaAvroDeserializer. mock-reg {"schema.registry.url" "test.reg"})
+          bytes (->> (ea/as-avro {:yolo {:string "yolo"}} schema)
+                     (.serialize serializer "yolo"))]
+
+      (is (instance? GenericData$Record (.deserialize deserializer "yolo" bytes)))))
+
+  (testing "can deserealize primitive"
+    (let [schema (ea/avro-schema "string")
+          mock-reg (MockSchemaRegistryClient.)
+          serializer (KafkaAvroSerializer. mock-reg {"schema.registry.url" "test.reg"})
+          deserializer (KafkaAvroDeserializer. mock-reg {"schema.registry.url" "test.reg"})
+          bytes (->> (ea/as-avro "yolo" schema)
+                     (.serialize serializer "yolo"))]
+
+      (is (= "yolo"
+             (.deserialize deserializer "yolo" bytes))))))

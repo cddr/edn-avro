006d273f05802205bd6d31f1487a5177d36ecd11
Andy Chambers <andy.chambers@agoradcm.com>
2023-02-06 01:33:25 +0000

    Update README

--- README.md
+++ README.md
@@ -7,49 +7,53 @@ Conversion between EDN and Avro GenericDatums (and back)
 
 ## Rationale
 
-In [jackdaw](https://github.com/fundingcircle/jackdaw), we made the
-mistake of getting into the business of maintaining a custom mapping
-between EDN and Avro. This meant we ended up duplicating a lot of code
-in order to marshall data from one format into another
+Clojure has an excellent library for serializing Clojure objects to avro in
+the form of abracad.avro. It works great for scenarios where you're writing
+the schema once in the header section of a file or something, and then for
+the rest of the records you just write out the raw bytes.
 
-https://github.com/FundingCircle/jackdaw/blob/master/src/jackdaw/serdes/avro.clj
+However when working with the confluent schema registry, you need to produce
+avro values that come in a container that includes the schema (the serializer
+can be configured to fail if the provided schema is not compatible with
+those that have been registered already on the same kafka topic).
 
-This means there have been (and likely remain) subtle bugs in the implementation
-which do not exist in the upstream implementation of avro. For example...
-
-  * https://github.com/FundingCircle/jackdaw/commit/26ec1e2fd14716ab923b17c43422dca5e6383484#diff-6ea591afdb2bef0213a7d11717398160
-  * https://github.com/FundingCircle/jackdaw/commit/eede5cc29b475bc2d24e4f1e9a8264a9c607438a#diff-6ea591afdb2bef0213a7d11717398160
-  * https://github.com/FundingCircle/jackdaw/commit/5b0661ec5f8a552c27de0e388db09abedea6f52a#diff-6ea591afdb2bef0213a7d11717398160
-
-This library aims to resolve these problems by using the ClojureDatumReader
-and ClojureDatumWriter defined in the abracad.avro project to define two
-methods `as-avro` and `as-edn` to translate between edn and avro.
-
-There might still be bugs in that library (and this one) but it leverages
-the Avro API a bit better so that there's less logic to implement.
+This library make it easy to wrap your clojure values in just such a container.
 
 Armed with a utility such as this, there is no longer a need to use custom
 implementations of KafkaAvroSerializer and Serde interfaces. Instead we just
 use the standard implmentations and convert our EDN data into a GenericDatum
-instance before returning sending it to a kafka producer or returning it in
-a kafka stream.
+instance before sending it to a kafka producer or returning it in a kafka stream.
 
 ## Usage
 
 ```
 (ns my.cool.app
   (:require
-    [cddr.edn-avro :as avro]))
+    [cddr.edn-avro :as avro])
+  (:import
+    (java.util Properties)
+    (org.apache.kafka.clients.producer KafkaProducer)))
 
-(def schema "string")
-(def msg "yolo")
+(def schema {:type "record"
+             :name "Foo"
+             :fields [{:name "aString" :type "string"}
+                      {:name "aUnion" :type ["string" "null"]}]})
 
-(def msg-as-avro (avro/as-avro msg {:schema schema}))
+(def cfg (doto (Properties.)
+           (.put "bootstrap.servers" "localhost:9092")
+           (.put "acks" "all")
+           (.put "key.serializer" "io.confluent.kafka.serializers.KafkaAvroSerializer")
+           (.put "value.serializer" "io.confluent.kafka.serializers.KafkaAvroSerializer")))
 
-(def msg-as-edn (avro/as-edn msg-as-avro {:schema schema}))
+(def p (KafkaProducer. cfg))
 
-msg-as-edn
-=> "yolo"
+@(.send p (as-avro {:aString "test-1"
+                    :aUnion "test-2"}
+                   {:schema schema}))
+
+@(.send p (as-avro {:aString "test-1"
+                    :aUnion nil}
+                   {:schema schema}))
 
 ```
 
--- src/cddr/edn_avro.clj
+++ src/cddr/edn_avro.clj
@@ -7,7 +7,7 @@
    (abracad.avro ClojureDatumWriter ClojureDatumReader)
    (java.nio ByteBuffer)
    (java.io ByteArrayInputStream ByteArrayOutputStream DataInputStream)
-   (org.apache.avro Schema$Parser Schema$Type)
+   (org.apache.avro Schema Schema$Parser Schema$Type)
    (org.apache.avro.generic GenericData GenericData$Record
                             GenericRecordBuilder GenericDatumWriter GenericDatumReader)
    (org.apache.avro.io
@@ -21,48 +21,57 @@
 
 (defn as-avro
   [object {:keys [schema]}]
-  (let [cdr (ClojureDatumWriter. schema)
+  (let [schema-w (if (instance? Schema schema)
+                   schema
+                   (avro-schema schema))
+        cdr (ClojureDatumWriter. schema-w)
         out (ByteArrayOutputStream.)
-        gdr (GenericDatumReader. schema)]
-    (.write cdr schema object (.directBinaryEncoder (EncoderFactory.) out nil))
+        gdr (GenericDatumReader. schema-w)]
+    (.write cdr schema-w object (.directBinaryEncoder (EncoderFactory.) out nil))
     (.read gdr nil (.binaryDecoder (DecoderFactory.) (.toByteArray out) nil))))
 
 (defn as-edn
-  [generic-record {:keys [schema]}]
-  (let [schema-w (.getSchema generic-record)
-        cdr (ClojureDatumReader. (or schema schema-w))
+  [generic-datum {:keys [schema]}]
+  (let [schema-w (.getSchema generic-datum)
+        schema-r (if (instance? Schema schema)
+                   schema
+                   (when schema
+                     (avro-schema schema)))
+        cdr (ClojureDatumReader. (or schema-r schema-w))
         gdw (GenericDatumWriter. schema-w)
         out (ByteArrayOutputStream.)
         df (DecoderFactory.)]
 
-    (.write gdw generic-record (.directBinaryEncoder (EncoderFactory.) out nil))
+    (.write gdw generic-datum (.directBinaryEncoder (EncoderFactory.) out nil))
     (let [gr-bytes (.toByteArray out)
-          decoder (cond->> (.directBinaryDecoder df (ByteArrayInputStream. (.toByteArray out)) nil)
-                    schema (.resolvingDecoder df schema-w schema))]
+          decoder (cond->> (.directBinaryDecoder df (ByteArrayInputStream.
+                                                     (.toByteArray out))
+                                                 nil)
+                    schema (.resolvingDecoder df schema-w schema-r))]
       (.read cdr nil decoder))))
 
 (comment
   ;; roundtrip a record (optionally using a custom reader schema)
-  (let [s1 (avro-schema {:type "record"
-                         :name "Foo"
-                         :fields [{:name "yolo"
-                                   :type ["string" "null"]}]})
-        s2 (avro-schema {:type "record"
-                         :name "Foo"
-                         :fields [{:name "yolo"
-                                   :type ["string" "null"]}
-                                  {:name "abc"
-                                   :type ["null" "long"]
-                                   :default nil}]})
+  (let [s1 {:type "record"
+            :name "Foo"
+            :fields [{:name "yolo"
+                      :type ["string" "null"]}]}
+        s2 {:type "record"
+            :name "Foo"
+            :fields [{:name "yolo"
+                      :type ["string" "null"]}
+                     {:name "abc"
+                      :type ["null" "long"]
+                      :default nil}]}
 
         avit (as-avro {:yolo "yolo"} {:schema s1})]
 
     (-> avit
-        (as-edn {:schema s2}))
+        (as-edn {:schema s2})))
     ;; {"yolo" {"string" "yolo"}, "abc" nil}
 
     (-> avit
-        (as-edn {})))
+        (as-edn {}))
     ;; {"yolo" {"string" "yolo"}}
 
   (let [s1 (avro-schema "string")

e515d0b8fff59b9e2ebe21534c195fef5030226a
Andy Chambers <andrew.chambers@statsbomb.com>
2023-02-06 22:10:56 +0000

    More build tweaking

--- build.clj
+++ build.clj
@@ -1,16 +1,26 @@
 (ns build
   (:require
+   [clojure.string :as str]
    [clojure.tools.build.api :as b]
    [deps-deploy.deps-deploy :as dd]))
 
+(def version
+  (let [git-tag (b/git-process {:git-args "describe --tags --abbrev=0"})
+        git-describe (b/git-process {:git-args "describe"})]
+    (str/join "-" (->> [git-tag (when-not (= git-tag git-describe)
+                                  "SNAPSHOT")]
+                       (remove nil?)))))
+
 (def lib 'cddr/edn-avro)
-(def version (b/git-process {:git-args "describe"}))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
 
 (defn clean [_]
   (b/delete {:path "target"}))
+
+(defn print-version [_]
+  (print version))
 
 (defn jar [_]
   (b/write-pom {:class-dir class-dir
